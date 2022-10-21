52a16e374bd5630187ecb235643c086047d6f960
andavas <andesonalmeida8@gmail.com>
2022-10-21 15:47:11 -0300

    Adição de tabela para exibição de resultados

---
+++ src/components/ResultTable/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import axios from "axios";
+import { Table } from 'antd';
+import { useAuthContext } from "../../context/authContext"
+import { useNavigate } from 'react-router-dom';
+
+
+  
+
+export default function ResultTable (props) {
+    const { token, loadingToken } = useAuthContext();
+    const navigate = useNavigate();
+    const [desafios, setDesafios] = React.useState([]);
+    const baseApi = "http://localhost:4000";
+
+    React.useEffect(async () => {
+        if (!loadingToken) {
+          if (token === null) {
+            navigate("/login");
+          } else {
+            axios.get(baseApi + '/api/games/', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }).then((response) => {
+                response.data.data.forEach((desafio) => {
+                    axios.get(baseApi + '/api/games/' + desafio.id, {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }).then((response) => {
+                        setDesafios(desafios => [...desafios, response.data])
+                    }).catch((error) => {
+                        console.log(error);
+                    })
+                }); console.table(desafios); // axios para cada challengeID | Adicionar campo se foi finalizado com ou sem erros | Adicionar campos para code e test corretos
+            }).catch((error) => {
+                console.log(error);
+            })
+          }
+        }
+      }, [loadingToken]);
+    
+      if (!token) {
+        return <></>;
+      }
+
+    // const columns = [
+    //     {
+    //       title: 'Desafio',
+    //       dataIndex: 'name',
+    //     },
+    //     {
+    //       title: 'Age',
+    //       dataIndex: 'age',
+    //     },
+    //     {
+    //       title: 'Address',
+    //       dataIndex: 'address',
+    //     },
+    //   ];
+    
+    return (
+        <Table dataSource={desafios} />
+    )}
--- src/pages/Resultados/index.jsx
+++ src/pages/Resultados/index.jsx
@@ -1,37 +1,120 @@
 import Header from "../../components/Header";
-import React from 'react';
+import ResultTable from "../../components/ResultTable";
+import React from "react";
 import axios from "axios";
-import { useAuthContext } from "../../context/authContext"
-import { RepeatOneSharp } from "@mui/icons-material";
+import { Table } from "antd";
+import { useAuthContext } from "../../context/authContext";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
-const Resultados = () => {
-    const {token, userInfo} = useAuthContext();
-    const [desafios, setDesafios] = React.useState([]);
-    const baseApi = "http://localhost:4000";
-    React.useEffect(() => {
-        axios.get(baseApi + '/api/games/', {
+import "./Resultados.css";
+
+const Resultados = (props) => {
+  const { token, loadingToken } = useAuthContext();
+  const navigate = useNavigate();
+  const [desafios, setDesafios] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const baseApi = "http://localhost:4000";
+
+  React.useEffect(async () => {
+    if (!loadingToken) {
+      if (token === null) {
+        navigate("/login");
+      } else {
+        axios
+          .get(baseApi + "/api/games/", {
             headers: {
-                Authorization: `Bearer ${token}`,
+              Authorization: `Bearer ${token}`,
             },
-        }).then((response) => {
-            response.data.forEach((desafio) => {
-                axios.get(baseApi + '/api/games/' + desafio.id, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }).then((response) => {
-                    setDesafios(desafios => [...desafios, response.data])
-                }).catch((error) => {
-                    console.log(error);
+          })
+          .then((gamesRes) => {
+            gamesRes.data.forEach((desafio) => {
+              axios
+                .get(baseApi + "/api/games/" + desafio.id, {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
                 })
-            }); console.table(desafios); // axios para cada challengeID | Adicionar campo se foi finalizado com ou sem erros | Adicionar campos para code e test corretos
-        }).catch((error) => {
+                .then((gameRes) => {
+                  const element = gameRes.data;
+                  axios
+                    .get(baseApi + "/api/challenges/" + element.challengeID, {
+                      headers: {
+                        Authorization: `Bearer ${token}`,
+                      },
+                    })
+                    .then((challengeRes) => {
+                      console.log(challengeRes.data);
+                      Object.assign(element, {
+                        challengeName: challengeRes.data.name,
+                        difficulty:
+                          challengeRes.data.dificulty === 0
+                            ? "Fácil"
+                            : challengeRes.data.dificulty === 1
+                            ? "Médio"
+                            : "Difícil",
+                      });
+                      setDesafios((desafios) => [...desafios, element]);
+                    });
+                })
+                .catch((gameErr) => {
+                  console.log(gameErr);
+                });
+            });
+            // axios para cada challengeID | Adicionar campo se foi finalizado com ou sem erros | Adicionar campos para code e test corretos
+          })
+          .catch((error) => {
             console.log(error);
-        })
-    }, []);
-    return ( <div>
-        <Header/>
-    </div> );
-}
- 
+          });
+        setLoading(false);
+      }
+    }
+  }, [loadingToken]);
+
+  if (!token) {
+    return <></>;
+  }
+
+  const columns = [
+    {
+      title: "Desafio",
+      dataIndex: "challengeName",
+    },
+    {
+      title: "Dificuldade",
+      dataIndex: "difficulty",
+    },
+    {
+      title: "Reloads",
+      dataIndex: "reloads",
+    },
+    {
+      title: "Pontuação",
+      dataIndex: "score",
+    },
+  ];
+
+  return (
+    <div id="resultadosConatiner">
+      <Header />
+      {loading ? (
+        <Spinner
+          style={{ margin: "0 0 10px 0" }}
+          animation="border"
+          variant="info"
+        />
+      ) : (
+        <div id="tableResultados">
+          <Table
+            title={() => "Resultados"}
+            bordered={true}
+            dataSource={desafios}
+            columns={columns}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Resultados;
